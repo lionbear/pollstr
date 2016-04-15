2ebd92e081e6e95451f6979898f023daa9be725f
Martin C. Richards <martinffx@gmail.com>
2016-04-15 22:06:46 +0200

    added logging

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ var Twit = require('twit'),
     repo = require('./lib/repo');
 
 try {
+
   var T = new Twit({
     consumer_key: process.env.TWITTER_CONSUMER_KEY,
     consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -12,11 +13,15 @@ try {
     access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
   });
 
+    console.log('Success: created twitter client');
+
   var stream = T.stream('statuses/filter',
                         { track: ['#anc', '#da', '#eff'],// 'economic freedom fighter',
                           //'african national congress', 'democratic alliance'],
                           filter_level: 'medium',
                           language: ['en','af','st','ve','zu','xh','tn','ts','nr','ss'] });
+
+    console.log('Suceess: created stream');
 
   stream.on('tweet', function (tweet) {
     try {
