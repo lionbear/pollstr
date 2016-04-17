37e6e5fa99c1ea55754ea8392afcafdcbcd69029
Martin C. Richards <martinffx@gmail.com>
2016-04-17 11:44:44 +0200

    exact replica of dokku in prod

--- src/lib/repo.js
+++ src/lib/repo.js
@@ -1,32 +1,14 @@
 'use strict';
 
-var cradle = require('cradle');
-
-cradle.setup({
-    host: process.env.COUCHDB_URL,
-    cache: true,
-    raw: false,
-    forceSave: true
-  });
-
-var c = new(cradle.Connection),
-    db = c.database('tweets');
-
-db.exists(function (err, exists) {
-  if (err) {
-    console.log('error', err);
-  } else if (exists) {
-    console.log('the force is with you.');
-  } else {
-    console.log('database does not exists.');
-    db.create();
-    /* populate design documents */
-  }
-});
+let nano = require('nano')('http://' + process.env.COUCHDB_URL);
 
 exports.save = function(tweet) {
-  db.save(tweet.id.toString(), tweet, function(err, resp){
-    if(err) console.error(err);
-    console.log(resp);
+    nano.insert(tweet, tweet.id.toString(), function(err, body, header) {
+        if(err) {
+            console.log(err);
+            return;
+        }
+        console.log(header);
+        console.log(body);
   });
 };
