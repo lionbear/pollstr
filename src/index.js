595364b3248123427c65e764c59133d9539d240b
Martin C. Richards <martinffx@gmail.com>
2016-04-23 10:01:06 +0200

    keep restarting process on failure

--- src/index.js
+++ src/index.js
@@ -4,34 +4,49 @@ var Twit = require('twit'),
     R = require('ramda'),
     repo = require('./lib/repo');
 
-try {
+while(true) {
+    try {
+        let stream = process();
+    } catch(ex){
+        console.log('========================');
+        console.log('Error: unexpected error');
+        console.log('========================');
+        console.log(ex);
+        console.log('========================');
+        stream.stop();
+    }
+}
 
-  var T = new Twit({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    access_token: process.env.TWITTER_ACCESS_TOKEN,
-    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-  });
+
+function process() {
+    var T = new Twit({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token: process.env.TWITTER_ACCESS_TOKEN,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
 
     console.log('Success: created twitter client');
 
-  var stream = T.stream('statuses/filter',
-                        { track: ['#anc', '#da', '#eff'],// 'economic freedom fighter',
-                          //'african national congress', 'democratic alliance'],
-                          // filter_level: 'medium',
-                          language: ['en','af','st','ve','zu','xh','tn','ts','nr','ss'] });
+    var stream = T.stream('statuses/filter',
+                          { track: ['#anc', '#da', '#eff'],// 'economic freedom fighter',
+                            //'african national congress', 'democratic alliance'],
+                            // filter_level: 'medium',
+                            language: ['en','af','st','ve','zu','xh','tn','ts','nr','ss'] });
 
     console.log('Suceess: created stream');
 
-  stream.on('tweet', function (tweet) {
-    try {
-      repo.save(tweet);
-    } catch(ex) {
-        console.log('Error: stream error');
-      console.log(ex);
-    }
-  });
-} catch(ex){
-    console.log('Error: unexpected error');
-  console.log(ex);
+    stream.on('tweet', function (tweet) {
+        try {
+            repo.save(tweet);
+        } catch(ex) {
+            console.log('========================');
+            console.log('Error: stream error');
+            console.log('========================');
+            console.log(ex);
+            console.log('========================');
+        }
+    });
+
+    return stream;
 }
